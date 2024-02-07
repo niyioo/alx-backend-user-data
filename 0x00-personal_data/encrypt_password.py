65173e63299c2de9906c5ce48123c8971065279c
niyioo <niyiolagbegi@gmail.com>
2024-02-07 11:51:29 +0100

    task 6

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Module for encrypting passwords.
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if a password matches its hashed version.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The password to check.
+
+    Returns:
+        bool: True if the password matches
+        the hashed password, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+
+if __name__ == "__main__":
+    # Example usage
+    password = "MyAmazingPassw0rd"
+    encrypted_password = hash_password(password)
+    print(encrypted_password)
+    print(is_valid(encrypted_password, password))
