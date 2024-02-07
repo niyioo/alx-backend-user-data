5b5f75fddbbaa71133c0b28d833072e55d245d23
niyioo <niyiolagbegi@gmail.com>
2024-02-07 11:58:27 +0100

    fx

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -34,11 +34,3 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
         the hashed password, False otherwise.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
-
-if __name__ == "__main__":
-    # Example usage
-    password = "MyAmazingPassw0rd"
-    encrypted_password = hash_password(password)
-    print(encrypted_password)
-    print(is_valid(encrypted_password, password))
