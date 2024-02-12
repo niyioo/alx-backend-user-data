a0d4c91410f7cb341934e28b8c8ff8c62f7a21e4
niyioo <niyiolagbegi@gmail.com>
2024-02-12 11:58:14 +0100

    task 5. Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,13 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Return authorization header"""
-        return None
+        if request is None:
+            return None
+
+        if "Authorization" not in request.headers:
+            return None
+
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Return current user"""
