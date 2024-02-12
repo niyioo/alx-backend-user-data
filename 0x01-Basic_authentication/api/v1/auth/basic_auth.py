b309e30a67f09dfc359a419cc95640d98808789d
niyioo <niyiolagbegi@gmail.com>
2024-02-12 12:29:42 +0100

    Task 7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,27 @@ class BasicAuth(Auth):
     """
     Basic authentication class
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header
+        for Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization header.
+
+        Returns:
+            str: The Base64 part of the Authorization header,
+            or None if the header is invalid.
+        """
+        if (
+            authorization_header is None
+            or not isinstance(authorization_header, str)
+        ):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split(" ", 1)[1]
