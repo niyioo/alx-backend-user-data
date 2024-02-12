2dc30dcd542b4d643e840f5b92726b9b9ffdc14f
niyioo <niyiolagbegi@gmail.com>
2024-02-12 12:46:13 +0100

    Task 8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Module that defines the BasicAuth class for basic authentication.
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -35,3 +36,28 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ", 1)[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 authorization header.
+
+        Args:
+            base64_authorization_header (str): The Base64 authorization header.
+
+        Returns:
+            str: The decoded value of the Base64 header,
+            or None if it's not valid.
+        """
+        if (
+            base64_authorization_header is None
+            or not isinstance(base64_authorization_header, str)
+        ):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except base64.binascii.Error:
+            return None
