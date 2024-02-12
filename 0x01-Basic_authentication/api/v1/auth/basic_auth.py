db4ae79745d50ebe7c01fc89fab69057e5cd29d9
niyioo <niyiolagbegi@gmail.com>
2024-02-12 12:53:16 +0100

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,3 +61,29 @@ class BasicAuth(Auth):
             return decoded_str
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts user email and password from a decoded Base64
+        authorization header.
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded
+            Base64 authorization header.
+
+        Returns:
+            tuple: A tuple containing the user email and password.
+        """
+        if (
+            decoded_base64_authorization_header is None
+            or not isinstance(decoded_base64_authorization_header, str)
+        ):
+            return None, None
+
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+
+        user_email, user_password = decoded_base64_authorization_header.split(
+            ":", 1)
+        return user_email, user_password
