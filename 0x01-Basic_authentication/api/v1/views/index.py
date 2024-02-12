66df4fc5cee2e8931611aadaa5add6c1fcca2c94
niyioo <niyiolagbegi@gmail.com>
2024-02-12 11:15:22 +0100

    task 1 error handler 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    """Route to raise a 401 Unauthorized error"""
+    abort(401)
