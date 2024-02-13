0e260e2e43a5b34c59756b3483cb8aecd7ed66b8
niyioo <niyiolagbegi@gmail.com>
2024-02-13 10:44:01 +0100

    app

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,15 +26,15 @@ elif auth_type == "basic_auth":
 @app.before_request
 def before_request():
     if auth is None:
-        return
-
-    excluded = ['/api/v1/status/',
-                '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if auth.require_auth(request.path, excluded):
-        if auth.authorization_header(request) is None:
-            abort(401, description="Unauthorized")
-        if auth.current_user(request) is None:
-            abort(403, description="Forbidden")
+        pass
+    else:
+        excluded = ['/api/v1/status/',
+                    '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded):
+            if auth.authorization_header(request) is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description="Forbidden")
 
 
 @app.errorhandler(404)
