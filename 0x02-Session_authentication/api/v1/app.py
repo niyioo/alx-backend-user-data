b0326358375434a5f539df43b98b37e865e695c0
niyioo <niyiolagbegi@gmail.com>
2024-02-14 13:15:45 +0100

    fix app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,7 +33,7 @@ def before_request():
         if not auth.authorization_header(request):
             abort(401)
         request.current_user = auth.current_user(request)
-        if not auth.current_user(request):
+        if not request.current_user:
             abort(403)
 
 
