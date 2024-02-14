98ce814ac95d0b625a9fa90e9644d76f9bfa9180
niyioo <niyiolagbegi@gmail.com>
2024-02-14 14:46:33 +0100

    4. Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -42,3 +42,13 @@ class Auth:
     def current_user(self, request=None) -> User:
         """Return current user"""
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """
+        Returns a cookie value from a request
+        """
+        if request is None:
+            return None
+
+        session_name = getenv("SESSION_NAME", "_my_session_id")
+        return request.cookies.get(session_name)
