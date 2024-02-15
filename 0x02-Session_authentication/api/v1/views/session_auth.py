db69a1f7090865e66450cd66321338e0003cdc39
niyioo <niyiolagbegi@gmail.com>
2024-02-15 09:37:34 +0100

    fx

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,6 +6,7 @@ from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
 from api.v1.app import auth
+from api.v1.auth.session_auth import SessionAuth
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
