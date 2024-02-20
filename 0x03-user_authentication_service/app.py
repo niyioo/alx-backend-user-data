e9bc9e415afb6480b0b53b88027befd219815af2
niyioo <niyiolagbegi@gmail.com>
2024-02-20 13:00:45 +0100

    nnn

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,7 +87,6 @@ def get_reset_password_token() -> str:
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """Update user's password"""
-    
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
