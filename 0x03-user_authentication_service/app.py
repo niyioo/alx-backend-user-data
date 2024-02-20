c17453025404abfa1517d335ee3340b6c1163437
niyioo <niyiolagbegi@gmail.com>
2024-02-20 12:07:49 +0100

    v

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,16 @@ def logout():
         return jsonify({"message": "Forbidden"}), 403
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Retrieve user profile."""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        return "Forbidden", 403
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
